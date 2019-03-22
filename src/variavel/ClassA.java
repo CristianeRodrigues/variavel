c934c90bcf25d070449584c1862f5f9693566185
cristiane <cris_csr@yahoo.com.br>
2019-03-21 22:14:13 -0300

    first commit

---
+++ src/variavel/ClassA.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package variavel;
+
+/**
+ *
+ * @author Cristiane
+ */
+public class ClassA {
+    
+    int codigoA;
+    String nomeA;
+    
+    public void soma(){
+        int x;
+        
+    }
+  
+    
+}
